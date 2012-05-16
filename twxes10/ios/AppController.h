9bcf191a56d1e5a1e17ed90072ac79f03e66c904
Di Wu <diwwu@zynga.com>
2012-05-16 16:59:56 +0800

    Initial Commit

---
+++ twxes10/ios/AppController.h
@@ -0,0 +1,17 @@
+//
+//  twxes10AppController.h
+//  twxes10
+//
+//  Created by diwwu on 5/16/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController	*viewController;
+}
+
+@end
+
---
+++ twxes10/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  twxes10AppController.h
+//  twxes10
+//
+//  Created by diwwu on 5/16/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
